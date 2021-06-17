2a40f5de5ef28447194ce180f9f0ab0ef356fb10
Micaela21 <mica_2106@hotmail.com>
2021-06-17 11:43:43 -0300

    multiple agents

--- back/Test/seleniumTest.js
+++ back/Test/seleniumTest.js
@@ -1,7 +1,6 @@
 const webdriver = require("selenium-webdriver"),
   By = webdriver.By,
   until = webdriver.until;
-
 const chrome = require("selenium-webdriver/chrome");
 const chromedriver = require("chromedriver");
 
@@ -15,15 +14,21 @@ var driver = new webdriver.Builder()
   .setChromeOptions(options)
   .build();
 
-driver.get("http://www.google.com").then(() => {
-  var q = driver.findElement(By.name("q"));
-  console.log(q)
-  q.sendKeys("webdriver")
-  driver.quit();
-});
-
-// driver.get("https://c49c14a3c41f.ngrok.io").then(() => {
-//   driver.findElement(By.name("search")).sendKeys("zapatillas")
-//   driver.findElement(By.name('button')).click()
+// driver.get("http://www.google.com").then(() => {
+//   var q = driver.findElement(By.name("q"));
+//   console.log(q)
+//   q.sendKeys("webdriver")
 //   driver.quit();
 // });
+
+async function test(){
+  let page = await driver.get("https://c49c14a3c41f.ngrok.io")
+  try {
+    page && await driver.findElement(By.name("search")).sendKeys("zapatillas")
+    page && await driver.findElement(By.name('button')).click()
+  } catch(e){
+    console.log(e)
+  }
+  driver.quit();
+
+}
