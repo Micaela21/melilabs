0527613532a7c71a4487c00eab4de03c38ed2979
Micaela21 <mica_2106@hotmail.com>
2021-06-18 11:21:33 -0300

    multiple agents

--- back/Test/seleniumTest.js
+++ back/Test/seleniumTest.js
@@ -15,14 +15,14 @@ const driver = new webdriver.Builder()
   .build();
 
 async function test(){
-  let page = await driver.get(process.env.URL)
+  let page = await driver.get('http://192.168.200.35:3001/')
   try {
     page && await driver.findElement(By.name("search")).sendKeys("zapatillas")
     page && await driver.findElement(By.name('button')).click()
   } catch(e){
     console.log(e)
   }
-  
+
   let products = await driver.wait(until.elementIsVisible(By.name('mayor')),5000)
   try {
     products && await driver.findElement(By.name('mayor')).click()
