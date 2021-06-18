25d29d0dce9e0f11eb3b43f23305a5eccecd52f1
Micaela21 <mica_2106@hotmail.com>
2021-06-18 15:43:38 -0300

    multiple agents

--- back/Test/seleniumTest.js
+++ back/Test/seleniumTest.js
@@ -3,6 +3,7 @@ const webdriver = require("selenium-webdriver"),
   until = webdriver.until;
 const chrome = require("selenium-webdriver/chrome");
 const chromedriver = require("chromedriver");
+const timeunit = require('timeunit')
 
 chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
 
@@ -14,27 +15,32 @@ const driver = new webdriver.Builder()
   .setChromeOptions(options)
   .build();
 
-  driver.get('https://93a67af8a987.ngrok.io').then(response => {
-    console.log(response)
-  })
+  // driver.get('https://93a67af8a987.ngrok.io').then(response => {
+  //   console.log(response)
+  //   driver.findElement(By.name("search")).sendKeys("zapatillas")
+  //   driver.findElement(By.name('button')).click().then(response => {
+  //     console.log(response)
+  //     driver.findElement(By.name("mayor")).click()
+  //   })
+  // })
 
 async function test(){
-  let page = await driver.get('https://a1d5ca30dd0f.ngrok.io').then(response => {
-    console.log(response)
-  })
+  let page = await driver.get('https://93a67af8a987.ngrok.io')
   try {
     page && await driver.findElement(By.name("search")).sendKeys("zapatillas")
     page && await driver.findElement(By.name('button')).click()
+    driver.manage().timeouts().pageLoadTimeout(20, timeunit.seconds);
+    console.log(driver.manage().timeouts().pageLoadTimeout(20, timeunit.seconds))
+    console.log('wait')
+    let products = await driver.wait(driver.findElement(By.xpath("/html/body/div/div/div/div[2]/div[2]/div/div/button[2]")),5000)
+    console.log('click')
+    products && await driver.findElement(By.xpath("/html/body/div/div/div/div[2]/div[2]/div/div/button[2]")).click()
   } catch(e){
     console.log(e)
   }
 
-  try {
-    let products = await driver.wait(until.elementIsVisible(By.name('mayor')),5000)
-    products && await driver.findElement(By.name('mayor')).click()
-  } catch (e) {
-    console.log(e)
-  }
   driver.quit();
 }
 
+
+test()
