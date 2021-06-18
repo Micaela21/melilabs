8f231cdd7a3542c9adcaf269a0d07f6f6e2bdcab
Micaela21 <mica_2106@hotmail.com>
2021-06-18 11:10:35 -0300

    multiple agents

--- back/index.js
+++ back/index.js
@@ -1,27 +1,27 @@
 const express = require("express");
 const morgan = require("morgan");
-// const cors = require('cors')
+const cors = require('cors')
 const search = require("./search");
 const path = require("path");
-// const helmet = require("helmet");
-// const server1 = express();
-// server1.disable("x-powered-by");
+const helmet = require("helmet");
+const server1 = express();
+server1.disable("x-powered-by");
 const server = express();
 
-// const corsOptions = {
-//   origin: 'http://localhost:3001',
-//   credentials : true
-// }
+const corsOptions = {
+  origin: 'http://localhost:3001',
+  credentials : true
+}
 
-// server.use(helmet.hidePoweredBy());
-// server.use(cors(corsOptions));
-// server.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, PATCH');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
+server.use(helmet.hidePoweredBy());
+server.use(cors(corsOptions));
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, PATCH');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 server.use(express.static("build"));
 server.use(morgan("dev"));
 server.use(express.urlencoded({ extended: false }));
