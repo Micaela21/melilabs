cbe772b8b2024755011abd45886eb9413cba5c23
Micaela21 <mica_2106@hotmail.com>
2021-06-18 12:43:07 -0300

    multiple agents

--- back/index.js
+++ back/index.js
@@ -1,52 +1,38 @@
 const express = require("express");
-const { createProxyMiddleware } = require('http-proxy-middleware');
 const morgan = require("morgan");
+const cors = require('cors')
 const search = require("./search");
 const path = require("path");
-const cors = require('cors')
 const helmet = require("helmet");
 const server1 = express();
 server1.disable("x-powered-by");
 const server = express();
-server.use(helmet.hidePoweredBy());
-// Configuration
-const API_SERVICE_URL = "https://a1d5ca30dd0f.ngrok.io";
 
 const corsOptions = {
-  origin: API_SERVICE_URL,
+  origin: 'http://localhost:3001',
   credentials : true
 }
 
-// Proxy endpoints
-server.use('/json_placeholder', createProxyMiddleware({
-  target: API_SERVICE_URL,
-  changeOrigin: true,
-  pathRewrite: {
-      [`^/json_placeholder`]: '',
-  },
-}));
-
+server.use(helmet.hidePoweredBy());
 server.use(cors(corsOptions));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', API_SERVICE_URL);
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, PATCH');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-
 server.use(express.static("build"));
 server.use(morgan("dev"));
 server.use(express.urlencoded({ extended: false }));
 server.use(express.json());
-
 server.use("/api", search);
 
 server.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/build/index.html"));
 });
 
-const PORT = API_SERVICE_URL || 3001;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
