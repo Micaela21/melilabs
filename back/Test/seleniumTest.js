d6e1ff8389fba380899439287b79f171d814be89
Micaela21 <mica_2106@hotmail.com>
2021-07-13 16:49:56 -0300

    multiple agents

--- back/Test/seleniumTest.js
+++ back/Test/seleniumTest.js
@@ -21,16 +21,11 @@ async function test(){
     page && await driver.findElement(By.name("search")).sendKeys("zapatillas")
     page && await driver.findElement(By.name('button')).click()
     console.log('wait')
-    let ele = await driver.wait(until.elementLocated(By.name('mayor')),50000);
-    let foo = await ele.getText();
-    assert(foo == "Hello from JavaScript");
-    // driver.wait(until.elementLocated(By.xpath('//*[@id="root"]/div/div/div[2]/div[2]/div/div/button[2]')), 10 * 1000).click()
     driver.quit();
   } catch(e){
     console.log(e)
     driver.quit();
   }
 }
-
 
 test()
