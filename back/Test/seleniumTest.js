b93e9c59116268e984b45448927cb6d36f3a8ac2
Micaela21 <mica_2106@hotmail.com>
2021-07-13 18:21:54 -0300

    multiple agents

--- back/Test/seleniumTest.js
+++ back/Test/seleniumTest.js
@@ -18,10 +18,12 @@ async function test(){
   let page = await driver.get('http://192.168.200.35:3001/')
   console.log(page)
   try {
-    const searchBar = await driver.findElement(By.name("search")).sendKeys("zapatillas")
-    const button = await driver.findElement(By.name('button')).click()
+    const searchBar = await driver.findElement(By.name("search"))
+    const button = await driver.findElement(By.name('button'))
     console.log(searchBar)
+    searchBar.sendKeys("zapatillas")
     console.log(button)
+    button.click()
     driver.quit();
   } catch(e){
     console.log(e)
